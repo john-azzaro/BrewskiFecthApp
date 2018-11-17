2bb0d0cef17acda7e8c66874d568daa45b5c899a
John Azzaro <jazzaro@temple.edu>
2018-11-16 20:00:18 -0800

    added js

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 "use strict";
+
+function getBreweries(state) {
+    const settings = {
+      url: `https://api.openbrewerydb.org/breweries?by_state=${state}`,
+      dataType: 'json',
+      type: 'GET',
+      success: displayResults,
+      error: function(err) {
+        console.error(err);
+      }
+    }
+    $.ajax(settings);
+  }
+  
+  function displayResults(responseJson) {
+    const breweries = responseJson.map((brewery) => {
+      return `
+        <li>
+          <a href="${brewery.website_url}">${brewery.name}</a>
+        </li>
+      `
+    }).join("\n");     // .join("\n") makes a new line 
+    $('.js-search-results').html(`<ul>${breweries}</ul>`);
+  }
+  
+  function watchForm() {
+    $('form').submit(function(event) {
+      event.preventDefault();
+      const state = $('#searchstate').val();
+      getBreweries(state, displayResults);
+      $('#searchstate').val("");
+    });
+  }
+  
+  $(watchForm);
